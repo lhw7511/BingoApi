b9380f8e9f7b505237cf70e4514ca569e3968f46
leehyeonuk <hwlee@beintech.co.kr>
2023-02-03 16:18:23 +0900

    aws s3 setting

--- src/main/java/com/project/BingoApi/jpa/controller/TestController.java
+++ src/main/java/com/project/BingoApi/jpa/controller/TestController.java
@@ -1,10 +1,15 @@
 package com.project.BingoApi.jpa.controller;
 
+import com.project.BingoApi.aws.component.AmazonS3Component;
+import com.project.BingoApi.aws.dto.FileDto;
 import com.project.BingoApi.jpa.domain.Member;
 import com.project.BingoApi.jpa.service.TestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -14,6 +19,7 @@ public class TestController {
 
     private final TestService testService;
 
+    private  final AmazonS3Component amazonS3Component;
     @GetMapping("test")
     public Member test(){
         return testService.save("testName");
@@ -22,5 +28,10 @@ public class TestController {
     @GetMapping("queryDslTest")
     public List<Member> queryDslTest(Long id) throws  Exception{
         return testService.getMemberList(id);
+    }
+
+    @PostMapping("fileUploadTest")
+    public FileDto fileUploadTest(@RequestPart("file") MultipartFile multipartFile){
+        return amazonS3Component.save(multipartFile);
     }
 }
