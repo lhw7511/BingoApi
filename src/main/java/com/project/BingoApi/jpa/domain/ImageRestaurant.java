21160744b443f72f0daa93e6ac5176d4201d7420
leehyeonuk <hwlee@beintech.co.kr>
2023-02-10 13:28:45 +0900

    domain test

--- src/main/java/com/project/BingoApi/jpa/domain/ImageRestaurant.java
+++ src/main/java/com/project/BingoApi/jpa/domain/ImageRestaurant.java
@@ -9,13 +9,13 @@ import javax.persistence.*;
 import static javax.persistence.FetchType.LAZY;
 
 @Entity
-@Table(name = "images_restaurant")
+@Table(name = "image_restaurant")
 @Getter
 @NoArgsConstructor
 public class ImageRestaurant {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "images_restaurant_id")
+    @Column(name = "image_restaurant_id")
     private Long id;
 
     @ManyToOne(fetch = LAZY)
@@ -26,6 +26,8 @@ public class ImageRestaurant {
     @Column(name = "image_url")
     private String imageUrl;
 
+    @Column(name = "image_key")
+    private String imageKey;
 
     public void setRestaurant(Restaurant restaurant){
         this.restaurant = restaurant;
--- src/main/java/com/project/BingoApi/jpa/repository/RestaurantRepository.java
+++ src/main/java/com/project/BingoApi/jpa/repository/RestaurantRepository.java
@@ -2,9 +2,13 @@ package com.project.BingoApi.jpa.repository;
 
 import com.project.BingoApi.jpa.domain.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface RestaurantRepository extends JpaRepository<Long, Restaurant> {
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+    @Query("select r from Restaurant r")
+    List<Restaurant> getList();
 
 }
