d25ed0aeef83578245acd3d749e80a9743f0780a
leehyeonuk <hwlee@beintech.co.kr>
2023-02-28 17:54:32 +0900

    불필요 로직 제거

--- src/main/java/com/project/BingoApi/config/SecurityConfig.java
+++ src/main/java/com/project/BingoApi/config/SecurityConfig.java
@@ -1,19 +1,14 @@
 package com.project.BingoApi.config;
 
-import com.project.BingoApi.filter.JwtFilter;
 import com.project.BingoApi.jpa.repository.UserRepository;
 import com.project.BingoApi.jwt.JwtAuthenticationFilter;
 import com.project.BingoApi.jwt.JwtAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -26,7 +21,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-       //http.addFilterBefore(new JwtFilter(), SecurityContextPersistenceFilter.class);
         http
                 .csrf()
                 .disable()
